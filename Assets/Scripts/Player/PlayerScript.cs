baec720a2ef6b15c1a33ec66b8489d9a644f31f9
AyaIbrahim3027 <ayaibrahim3027@gmail.com>
2024-03-28 01:48:44 +0200

    create PlayerScript

---
+++ Assets/Scripts/Player/PlayerScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    [Header("Player Movement")]
+    public float playerSpeed =1.1f;
+
+    [Header("Player Animator & Gravity")]
+    public CharacterController cc;
+
+    private void Update(){
+        playerMove();
+    }
+
+    void playerMove(){
+        float horizotal_axis = Input.GetAxisRaw("Horizontal");
+        float vertical_axis = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizotal_axis,0f,vertical_axis).normalized;
+        if (direction.magnitude >= 0.1f){
+            cc.Move(direction.normalized * playerSpeed*Time.deltaTime);
+
+        }
+    }
+    
+}
